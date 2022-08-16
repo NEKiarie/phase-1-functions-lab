446745b013bfce6c51a20c7f228e45f97afd9bbb
Elvis <s49188705@gmail.com>
2022-08-16 19:38:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const scuberHeadquarters = 42;
+function distanceFromHqInBlocks(location){
+    return Math.abs(location - scuberHeadquarters);
+}
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+   
+// function distanceTravelledInFeet(start, final){
+//     if (start > final){
+//         return (start - final) * 264;
+//     }
+//     else{
+//         return(final - start) * 264;
+//     }
+
+// }
+
+function distanceTravelledInFeet(start, final){
+    return Math.abs (final- start) * 264;
+}
+
+function calculatesFarePrice(start, final){
+    let feet = distanceTravelledInFeet(start, final);
+    if (feet < 400){
+        return 0;
+    }
+    else if( feet > 400 && feet < 2000){
+        return  ((feet - 400) * 2 / 100) ;
+    }
+    else if (feet > 2000 && feet <=2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
